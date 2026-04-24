4878fad6c2482da0bf873683bbafbd10f6b5033f
danyscor13-netizen <danyscor13@gmail.com>
2026-04-24 19:37:14 +0200

    Render home.html instead of returning a string

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 import os
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Funziona!"
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
