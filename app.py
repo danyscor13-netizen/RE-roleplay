0f1d93b1ca3f59596872af775f36d7fa0a0b479c
danyscor13-netizen <danyscor13@gmail.com>
2026-04-24 19:30:17 +0200

    Initialize Flask application with home route

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return "Funziona!"
+
+if __name__ == '__main__':
+  app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
